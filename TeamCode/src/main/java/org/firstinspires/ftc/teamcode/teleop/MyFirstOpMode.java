bf8bf1650e89640b306c8be85cbc424ed6d787f3
Cooper-L256 <c2g-cooper@outlook.com>
2023-09-18 16:10:10 -0700

    Set up basing LinearOpMode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MyFirstOpMode.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class MyFirstOpMode extends LinearOpMode {
+
+    public void runOpMode() throws InterruptedException {
+
+        //init code (Initializing Hardware, Adding telemetry data, Setting motor direction)
+
+        waitForStart();
+
+        //code after init (Running Code)
+
+    }
+}
