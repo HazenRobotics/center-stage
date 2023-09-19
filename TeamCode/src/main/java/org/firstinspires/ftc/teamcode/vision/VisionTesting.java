6fb9ff99c34ac1ad06f3881d4ec9a7d82397c876
MrFloofers0 <zachattack197611@gmail.com>
2023-09-18 18:26:20 -0700

    Vision Test - First push

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/VisionTesting.java
@@ -0,0 +1,94 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class VisionTesting extends OpenCvPipeline {
+
+    Telemetry telemetry;
+    Mat mat = new Mat();
+
+    PiecePosition piecePosition;
+
+
+    public enum PiecePosition {
+        LEFT,
+        RIGHT,
+        MIDDLE,
+        NOT_FOUND
+    }
+
+    static final Rect leftPos = new Rect(
+            new Point(0, 0),
+            new Point(426, 720));
+    static final Rect midPos = new Rect(
+            new Point(426, 0),
+            new Point(852, 720));
+    static final Rect rightPos = new Rect(
+            new Point(852, 0),
+            new Point(1278, 720));
+
+    public VisionTesting(Telemetry t) {
+        telemetry = t;
+    }
+
+
+    public Mat processFrame(Mat input, String type) {
+
+        Scalar colorLowerBound;
+        Scalar colorUpperBound;
+
+        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
+        if (type.equals("Red")) {
+            colorLowerBound = new Scalar(0, 40, 40);
+            colorUpperBound = new Scalar(20, 255, 255);
+        } else {
+            colorLowerBound = new Scalar(200, 40, 40);
+            colorUpperBound = new Scalar(210, 255, 255);
+        }
+        final double percentColorThreshold = 0.02;
+        Core.inRange(mat, colorLowerBound, colorUpperBound, mat);
+
+        Mat left = mat.submat(leftPos);
+        Mat middle = mat.submat(midPos);
+        Mat right = mat.submat(rightPos);
+
+        double leftValue = Core.sumElems(left).val[0] / leftPos.area() / 255;
+        left.release();
+
+        if (leftValue > percentColorThreshold) {
+            piecePosition = PiecePosition.LEFT;
+        } else {
+
+            double middleValue = Core.sumElems(middle).val[0] / midPos.area() / 255;
+            middle.release();
+
+            if (middleValue > percentColorThreshold) {
+                piecePosition = PiecePosition.MIDDLE;
+            } else {
+
+                double rightValue = Core.sumElems(right).val[0] / rightPos.area() / 255;
+                right.release();
+
+                if (rightValue > percentColorThreshold) {
+                    piecePosition = PiecePosition.RIGHT;
+                } else {
+                    piecePosition = PiecePosition.NOT_FOUND;
+                }
+            }
+        }
+
+
+        return null;
+    }
+    @Override
+    public Mat processFrame(Mat input) {
+        return null;
+    }
+    }
