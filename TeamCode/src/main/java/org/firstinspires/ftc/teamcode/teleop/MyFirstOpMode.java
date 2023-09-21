6b8b4b16880e257c0a7ee665a5b535d0fe20ee41
CooperL_256 <c2g-cooper@outlook.com>
2023-09-21 15:22:44 -0700

    Move Forward And Stop OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MyFirstOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MyFirstOpMode.java
@@ -1,16 +1,57 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import static java.lang.Thread.sleep;
 
-public class MyFirstOpMode extends LinearOpMode {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class MyFirstOpMode extends OpMode {
+
+    DcMotor frontLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backLeftMotor;
+    DcMotor backRightMotor;
 
     public void runOpMode() throws InterruptedException {
+        init();
+        moveForwardAndStop();
+    }
 
-        //init code (Initializing Hardware, Adding telemetry data, Setting motor direction)
+    @Override
+    public void init() {
+        // code before start (Initializing Hardware, Adding telemetry data, Setting motor direction)
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "front_left_motor");
+        frontRightMotor = hardwareMap.get(DcMotor.class,"front_right_motor"));
+        backLeftMotor = hardwareMap.get(DcMotor.class, "back_left_motor");
+        backRightMotor = hardwareMap.get(DcMotor.class,"back_right_motor"));
 
-        waitForStart();
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        //code after init (Running Code)
+        telemetry.addData("Mode: ", "Waiting");
+        telemetry.update();
+    }
+
+    public void moveForwardAndStop() throws InterruptedException {
+        telemetry.addData("Mode: ","Running");
+        telemetry.update();
+
+        frontLeftMotor.setPower(0.25);
+        frontRightMotor.setPower(0.25);
+        backLeftMotor.setPower(0.25);
+        backRightMotor.setPower(0.25);
+
+        Thread.sleep(2000);
+
+        frontLeftMotor.setPower(0.0);
+        frontRightMotor.setPower(0.0);
+        backLeftMotor.setPower(0.0);
+        backRightMotor.setPower(0.0);
+    }
+
+    @Override
+    public void loop() {
 
     }
 }
