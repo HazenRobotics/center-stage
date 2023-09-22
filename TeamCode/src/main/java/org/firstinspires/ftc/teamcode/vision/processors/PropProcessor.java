fd263e6061032c4acd07bda8755e2cc9eefec79f
Camden Harris <camdenharris@gmail.com>
2023-09-21 17:12:17 -0700

    processor works, need to figure out how to make a util and a opmode for it

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/processors/PropProcessor.java
@@ -0,0 +1,160 @@
+package org.firstinspires.ftc.teamcode.vision.processors;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+public class PropProcessor implements VisionProcessor {
+
+	public enum PropPosition {
+		LEFT, RIGHT, MIDDLE, NOT_FOUND
+	}
+	public enum PropColor {
+		RED, BLUE
+	}
+
+	PropColor propColor;
+	PropPosition propPosition;
+
+	private final Rect leftPos = new Rect( 0, 0, 420, 720 );
+	private final Rect midPos = new Rect( 424, 0, 420, 720 );
+	private final Rect rightPos = new Rect( 848, 0, 420, 720 );
+
+	@Override
+	public void init( int width, int height, CameraCalibration calibration ) {
+	}
+
+	@Override
+	public Object processFrame( Mat frame, long captureTimeNanos ) {
+		Imgproc.cvtColor( frame, frame, Imgproc.COLOR_RGB2HSV );
+
+		Scalar redLowerBound = new Scalar( 0, 100, 40 );
+		Scalar redUpperBound = new Scalar( 5, 255, 255 );
+		Scalar redLowerBound2 = new Scalar( 175, 100, 40 );
+		Scalar redUpperBound2 = new Scalar( 180, 255, 255 );
+
+		Scalar blueLowerBound = new Scalar( 100, 60, 40 );
+		Scalar blueUpperBound = new Scalar( 130, 255, 255 );
+
+		final double percentColorThreshold = 0.02 * 255; //Percent value on left, 255 for max amount of color
+
+		Mat matRed = new Mat( );
+		Mat matRed2 = new Mat( );
+		Mat matBlue = new Mat( );
+
+		Core.inRange( frame, redLowerBound, redUpperBound, matRed );
+		Core.inRange( frame, redLowerBound2, redUpperBound2, matRed2 );
+		Core.bitwise_or( matRed, matRed2, matRed );
+
+		Core.inRange( frame, blueLowerBound, blueUpperBound, matBlue );
+
+		if (Core.sumElems( matRed ).val[0] > Core.sumElems( matBlue ).val[0]) {
+			matRed.copyTo(frame);
+			propColor = PropColor.RED;
+		} else {
+			matBlue.copyTo(frame);
+			propColor = PropColor.BLUE;
+		}
+		matRed.release();
+		matBlue.release();
+
+		Mat left;
+		Mat middle;
+		Mat right;
+
+		left = frame.submat( leftPos );
+		middle = frame.submat( midPos );
+		right = frame.submat( rightPos );
+
+		double leftValue = Core.sumElems( left ).val[0] / leftPos.area( );
+		if( leftValue > percentColorThreshold ) {
+			left.release( );
+			middle.release( );
+			right.release( );
+			return PropPosition.LEFT;
+		}
+
+		double middleValue = Core.sumElems( middle ).val[0] / midPos.area( );
+		if( middleValue > percentColorThreshold ) {
+			left.release( );
+			middle.release( );
+			right.release( );
+			return PropPosition.MIDDLE;
+		}
+
+		double rightValue = Core.sumElems( right ).val[0] / midPos.area( );
+		if( rightValue > percentColorThreshold ) {
+			left.release( );
+			middle.release( );
+			right.release( );
+			return PropPosition.RIGHT;
+		}
+		left.release( );
+		middle.release( );
+		right.release( );
+		return PropPosition.NOT_FOUND;
+	}
+
+	@Override
+	public void onDrawFrame( Canvas canvas, int onscreenWidth, int onscreenHeight,
+							 float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext ) {
+		Paint selectedPaint = new Paint( );
+		selectedPaint.setColor( Color.GREEN );
+		selectedPaint.setStyle( Paint.Style.STROKE );
+		selectedPaint.setStrokeWidth( scaleCanvasDensity * 4 );
+
+		Paint nonSelectedPaint = new Paint( );
+		nonSelectedPaint.setColor( Color.RED );
+		nonSelectedPaint.setStyle( Paint.Style.STROKE );
+		nonSelectedPaint.setStrokeWidth( scaleCanvasDensity * 4 );
+
+		android.graphics.Rect drawRectangleLeft = makeGraphicsRect( leftPos, scaleBmpPxToCanvasPx );
+		android.graphics.Rect drawRectangleMiddle = makeGraphicsRect( midPos, scaleBmpPxToCanvasPx );
+		android.graphics.Rect drawRectangleRight = makeGraphicsRect( rightPos, scaleBmpPxToCanvasPx );
+
+		propPosition = (PropPosition) userContext;
+		switch( propPosition ) {
+			case LEFT:
+				canvas.drawRect( drawRectangleLeft, selectedPaint );
+				canvas.drawRect( drawRectangleMiddle, nonSelectedPaint );
+				canvas.drawRect( drawRectangleRight, nonSelectedPaint );
+				break;
+			case MIDDLE:
+				canvas.drawRect( drawRectangleLeft, nonSelectedPaint );
+				canvas.drawRect( drawRectangleMiddle, selectedPaint );
+				canvas.drawRect( drawRectangleRight, nonSelectedPaint );
+				break;
+			case RIGHT:
+				canvas.drawRect( drawRectangleLeft, nonSelectedPaint );
+				canvas.drawRect( drawRectangleMiddle, nonSelectedPaint );
+				canvas.drawRect( drawRectangleRight, selectedPaint );
+				break;
+			case NOT_FOUND:
+				canvas.drawRect( drawRectangleLeft, nonSelectedPaint );
+				canvas.drawRect( drawRectangleMiddle, nonSelectedPaint );
+				canvas.drawRect( drawRectangleRight, nonSelectedPaint );
+				break;
+		}
+	}
+
+	private android.graphics.Rect makeGraphicsRect( Rect rect, float scaleBmpPxToCanvasPx ) {
+		int left = Math.round( rect.x * scaleBmpPxToCanvasPx );
+		int top = Math.round( rect.y * scaleBmpPxToCanvasPx );
+		int right = left + Math.round( rect.width * scaleBmpPxToCanvasPx );
+		int bottom = top + Math.round( rect.height * scaleBmpPxToCanvasPx );
+
+		return new android.graphics.Rect( left, top, right, bottom );
+	}
+
+	public PropPosition getPiecePosition( ) {
+		return propPosition;
+	}
+}
