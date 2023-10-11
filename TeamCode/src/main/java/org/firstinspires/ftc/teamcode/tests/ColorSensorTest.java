dab1c3655faa3d1cf7de158ba346e18f3997e14b
Camden Harris <camdenharris@gmail.com>
2023-10-11 08:16:45 -0700

    testing color sensor, for whatever it's giving wacky values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ColorSensorTest.java
@@ -7,21 +7,22 @@ import android.view.View;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
 @TeleOp
 public class ColorSensorTest extends LinearOpMode {
-	RevColorSensorV3 color;
+	ColorSensor color;
 
 	@Override
 	public void runOpMode( ) throws InterruptedException {
-		color = hardwareMap.get( RevColorSensorV3.class, "color" );
+		color = hardwareMap.get( ColorSensor.class, "color" );
 
 		waitForStart();
 
 		while(opModeIsActive()) {
-			telemetry.addData( "red", color.red( ) );
-			telemetry.addData( "green", color.green( ) );
-			telemetry.addData( "blue", color.blue( ) );
+			telemetry.addData( "red", (double) color.red( ) );
+			telemetry.addData( "green", (double) color.green( ) );
+			telemetry.addData( "blue", (double) color.blue( ) );
 			telemetry.update( );
 		}
 	}
