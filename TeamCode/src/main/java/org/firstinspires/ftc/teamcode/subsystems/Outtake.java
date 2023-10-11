760c032432b235c085644b0afe1badeefa8259ce
victo <sirpoetvic@gmail.com>
2023-10-11 13:21:52 -0700

    need to move to camden's computer because adb bad

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Outtake {
+    Servo angler, release;
+
+    public enum ReleaseStates {
+        RETRACTED(0.582), DROP_ONE(0.636), EXTENDED(0.816);
+        private final double position;
+        private ReleaseStates (double pos) {
+            position = pos;
+        }
+        double getPosition() {
+            return position;
+        }
+    }
+
+    public Outtake(HardwareMap hw, String releaseName, String anglerName) {
+        release = hw.get(Servo.class, releaseName);
+        angler = hw.get(Servo.class, anglerName);
+    }
+
+    public void setReleasePosition (ReleaseStates state) {
+        release.setPosition(state.getPosition());
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/OuttakeTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.Outtake;
+
+@TeleOp
+public class OuttakeTest extends LinearOpMode {
+    Outtake outtake;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        outtake = new Outtake(hardwareMap, "angler", "release");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad1.a)
+                outtake.setReleasePosition( Outtake.ReleaseStates.EXTENDED );
+            else if (gamepad1.b)
+                outtake.setReleasePosition( Outtake.ReleaseStates.RETRACTED );
+            else if (gamepad1.y) {
+                outtake.setReleasePosition( Outtake.ReleaseStates.DROP_ONE );
+            }
+
+        }
+
+    }
+}
