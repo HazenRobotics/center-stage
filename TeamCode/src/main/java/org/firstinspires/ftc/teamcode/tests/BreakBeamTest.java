c89c09d8b30efef269a3a9ab9617a20339d46f61
Camden Harris <camdenharris@gmail.com>
2023-10-12 17:06:29 -0700

    breakbeam works great

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/BreakBeamTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/BreakBeamTest.java
@@ -2,19 +2,21 @@ package org.firstinspires.ftc.teamcode.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.teamcode.subsystems.IntakeBreakBeamSensor;
 
 @TeleOp
 public class BreakBeamTest extends LinearOpMode {
     IntakeBreakBeamSensor breakBeam;
+
     @Override
     public void runOpMode() throws InterruptedException {
         breakBeam = new IntakeBreakBeamSensor( hardwareMap, telemetry, "breakBeam");
         waitForStart();
 
         while (opModeIsActive()) {
-            telemetry.addData("value", breakBeam.getBeamState());
+            breakBeam.addTelemetry();
             telemetry.update();
         }
     }
