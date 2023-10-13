2892e257ca64605b681ed1a512753ea293f267b0
Camden Harris <camdenharris@gmail.com>
2023-10-12 23:05:34 -0700

    de-britishifying the code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ColorSensorTest.java
@@ -3,15 +3,15 @@ package org.firstinspires.ftc.teamcode.tests;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.subsystems.IntakeColourSensor;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeColorSensor;
 
 @TeleOp
 public class ColorSensorTest extends LinearOpMode {
-	IntakeColourSensor color;
+	IntakeColorSensor color;
 
 	@Override
 	public void runOpMode( ) throws InterruptedException {
-		color = new IntakeColourSensor(hardwareMap, telemetry, "color");
+		color = new IntakeColorSensor(hardwareMap, telemetry, "color");
 
 
 		waitForStart();
