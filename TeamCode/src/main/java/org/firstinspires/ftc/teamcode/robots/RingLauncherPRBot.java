e3549f50bec70f009e21401aa234525e2aeb5d05
Camden Harris <camdenharris@gmail.com>
2023-10-14 12:44:07 -0700

    getting ring launcher pr bot ready

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/RingLauncherPRBot.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.robots;
+
+public class RingLauncherPRBot {
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlywheelLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlywheelLauncher.java
@@ -9,8 +9,8 @@ public class FlywheelLauncher {
 
 //    can either be CR servos or motors, but is likely going to have to be a motor to control velocity
 //    and because CR servos have
-    CRServo left, right;
-//    DcMotorEx left, right;
+//    CRServo left, right;
+    DcMotorEx left, right;
 
     public FlywheelLauncher(HardwareMap hw) {
         this(hw, "left", "right", true, false);
@@ -18,14 +18,13 @@ public class FlywheelLauncher {
 
     public FlywheelLauncher (HardwareMap hw, String leftName, String rightName,
                              boolean leftReverse, boolean rightReverse) {
-        left = hw.get( CRServo.class, leftName );
-        right = hw.get( CRServo.class, rightName );
+//        left = hw.get( CRServo.class, leftName );
+//        right = hw.get( CRServo.class, rightName );
+        left = hw.get( DcMotorEx.class, leftName );
+        right = hw.get( DcMotorEx.class, rightName );
 
         left.setDirection( leftReverse ? DcMotorSimple.Direction.REVERSE : DcMotorSimple.Direction.FORWARD );
         right.setDirection( rightReverse ? DcMotorSimple.Direction.REVERSE : DcMotorSimple.Direction.FORWARD );
-
-//        left = hw.get( DcMotorEx.class, leftName );
-//        right = hw.get( DcMotorEx.class, rightName );
     }
 
     public void setPower( double power ) {
