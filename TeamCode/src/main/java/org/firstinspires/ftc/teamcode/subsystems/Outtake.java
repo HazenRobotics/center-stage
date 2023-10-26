cfdfb902a78a2cd6926b4ec97ca74308bfadb9ea
Cooper-L256 <c2g-cooper@outlook.com>
2023-10-26 16:19:31 -0700

    Added methods to Outtake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
@@ -1,15 +1,34 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Outtake {
     Servo retractionServo;
-    Servo rotationServo;      ervo;
+    Servo rotationServo;
 
     public Outtake() {
-        retractionServo = new Servo(hardwareMap.get(Servo.class, "retractionServo"));
-        rotationServo = new Servo(hardwareMap.get(Servo.class, "rotationServo"));
+        HardwareMap hw = new HardwareMap();
+        retractionServo = hw.get(Servo.class, "retractionServo");
+        rotationServo = hw.get(Servo.class, "rotationServo");
+    }
+
+
+    public void rotateServoUp() {
+        rotationServo.setPosition(1.0);
+    }
+    public void rotateDepositDown() {
+        rotationServo.setPosition(0.0);
+    }
+
+    public void extendOuttake() {
+        retractionServo.setPosition(1.0);
+    }
+    public void dropFirstPixel() {
+        retractionServo.setPosition(0.5);
+    }
+
+    public void dropSecondPixel() {
+        retractionServo.setPosition(0.0);
     }
 }
