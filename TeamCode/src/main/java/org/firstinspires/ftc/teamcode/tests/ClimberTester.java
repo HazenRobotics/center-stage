a5364cc7d3f110d8c4f54d1fb89576db1061c2da
Camden Harris <camdenharris@gmail.com>
2023-10-30 18:35:08 -0700

    climb created

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ClimberTester.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.Climber;
+
+@TeleOp
+public class ClimberTester extends LinearOpMode {
+
+	Climber climb;
+	@Override
+	public void runOpMode( ) throws InterruptedException {
+		climb = new Climber( hardwareMap );
+
+		waitForStart();
+
+		while (opModeIsActive()) {
+			climb.setPower( gamepad1.right_trigger - gamepad1.left_trigger );
+		}
+	}
+}
