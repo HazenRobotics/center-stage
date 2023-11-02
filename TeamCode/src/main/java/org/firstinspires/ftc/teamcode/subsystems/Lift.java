149a38f98d64b69d8cad2da89889b0cc8510d1b3
Armaan Sidhu <armaansidhu000@icloud.com>
2023-11-02 14:38:47 -0700

    we are turning the rotation of the deposit to be automatic; we are going to be using enums and encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
@@ -4,10 +4,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-enum Enum
+ enum Enum
 {
-     half;
-   //
+
+   HALF;
     public double halfwayUpTheLift()
     {
         return 0.5;
@@ -21,6 +21,8 @@ public class Lift {
     DcMotor liftMotor;
     Deposit deposit = new Deposit(HardwareMap);
     Telemetry telemetry;
+    //Change halfEnum to a different name
+    Enum halfEnum= Enum.HALF;
     public Lift(HardwareMap hw, Telemetry telemetry){ this(hw, telemetry, "lift_Motor" );}
     public Lift(HardwareMap hw, Telemetry telemetry, String liftName) {
 
@@ -31,8 +33,21 @@ public class Lift {
 
     public void setPower(double power) { liftMotor.setPower(power);}
 
-     public void rotateDeposit(double position){
-        if()
+     // we need to split up the rotation up and the rotation down
+     public void rotateDeposit(){
+
+         int rotatePress = 0;
+        if(liftMotor.getCurrentPosition() >= halfEnum.halfwayUpTheLift())
+        {
+            if(rotatePress == 0){
+                deposit.rotateDepositUp();
+                rotatePress++;
+                //rotate down
+            }else if(rotatePress == 1){
+                deposit.rotateDepositDown();
+                rotatePress = 0;
+            }
+        }
 
     }
 
