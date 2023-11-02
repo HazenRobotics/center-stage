2f5140c24b923df09073aec14ac451705fc1cd1a
Armaan Sidhu <armaansidhu000@icloud.com>
2023-11-02 15:00:22 -0700

    we are turning the rotation of the deposit to be automatic; we are going to be using enums and encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
@@ -4,16 +4,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
- enum Enum
-{
 
-   HALF;
-    public double halfwayUpTheLift()
-    {
-        return 0.5;
-    }
-
-}
 public class Lift {
     //Make an array because the servo
 
@@ -22,7 +13,7 @@ public class Lift {
     Deposit deposit = new Deposit(HardwareMap);
     Telemetry telemetry;
     //Change halfEnum to a different name
-    Enum halfEnum= Enum.HALF;
+
     public Lift(HardwareMap hw, Telemetry telemetry){ this(hw, telemetry, "lift_Motor" );}
     public Lift(HardwareMap hw, Telemetry telemetry, String liftName) {
 
@@ -34,10 +25,15 @@ public class Lift {
     public void setPower(double power) { liftMotor.setPower(power);}
 
      // we need to split up the rotation up and the rotation down
+    /*We want to have the rotation up of the depsoit to be automatic once the lift reaches half way up
+      To bring the depsoit back to its orginal state, we will use a teleOP
+
+     */
+
      public void rotateDeposit(){
 
          int rotatePress = 0;
-        if(liftMotor.getCurrentPosition() >= halfEnum.halfwayUpTheLift())
+        if(liftMotor.getCurrentPosition() >= 0.5)
         {
             if(rotatePress == 0){
                 deposit.rotateDepositUp();
