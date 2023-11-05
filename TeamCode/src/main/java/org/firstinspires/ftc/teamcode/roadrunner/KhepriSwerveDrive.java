864a1b1588037b7cfcb5de4ed7c85433ea9bdaab
Camden Harris <camdenharris@gmail.com>
2023-11-04 21:35:19 -0700

    swerve is rachetly tuned, this is so cursed, but it's gonna work for comp, i hope

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/KhepriSwerveDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/KhepriSwerveDrive.java
@@ -50,8 +50,8 @@ import java.util.List;
 @Config
 public class KhepriSwerveDrive extends SwerveDrive {
 
-	public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients( 0, 0, 0 );
-	public static PIDCoefficients HEADING_PID = new PIDCoefficients( 0, 0, 0 );
+	public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients( 8, 0, 0 );
+	public static PIDCoefficients HEADING_PID = new PIDCoefficients( 8, 0, 0 );
 
 	public static double VX_WEIGHT = 1;
 	public static double VY_WEIGHT = 1;
