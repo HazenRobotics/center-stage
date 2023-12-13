148031237da21ef22af23e091f97f38dfc323257
Armaan Sidhu <armaansidhu000@icloud.com>
2023-12-13 14:30:20 -0800

    Complete with RGB lights for signaling to human player

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RGBcontroller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RGBcontroller.java
@@ -1,15 +1,13 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
 import org.firstinspires.ftc.teamcode.utils.RGBLights;
 
 public class RGBcontroller extends RGBLights{
 
+
     private static final com.qualcomm.robotcore.hardware.HardwareMap HardwareMap = ;
     RGBLights rgbSignal;
-
 
     public RGBcontroller(String name)
     {
@@ -17,9 +15,23 @@ public class RGBcontroller extends RGBLights{
 
     }
 
-    public void setColor(RevBlinkinLedDriver.BlinkinPattern Color) {
-        rgbSignal.setPattern(Color);
+    public void setColor(RevBlinkinLedDriver.BlinkinPattern Color) {rgbSignal.setPattern(Color);}
+
+
+    public void autoColor()
+    {
+       // change colors every 5 seconds
+        // private static int everyLoop;
+
+            rgbSignal.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+
+            rgbSignal.setPattern(RevBlinkinLedDriver.BlinkinPattern.YELLOW);
+
+
+
+
     }
+
 
 
 }
