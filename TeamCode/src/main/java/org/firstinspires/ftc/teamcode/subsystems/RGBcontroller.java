1ff1bf904800e266c83932fc812f9b261cbd7f81
Armaan Sidhu <armaansidhu000@icloud.com>
2023-12-14 14:56:32 -0800

    Class for the LED lights. Can be used to set colors for the LEDs & create a cool pattern in auto(For now, it is just alternating between blue & yellow every five seconds, but will be changed to have the pattern say "8693" in binary

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RGBcontroller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RGBcontroller.java
@@ -1,9 +1,14 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static android.icu.lang.UProperty.MATH;
+import static java.lang.Thread.sleep;
+
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.utils.RGBLights;
+
 
 public class RGBcontroller extends RGBLights{
 
@@ -19,17 +24,26 @@ public class RGBcontroller extends RGBLights{
     public void setColor(RevBlinkinLedDriver.BlinkinPattern Color) {rgbSignal.setPattern(Color);}
 
 
-    public void autoColor()
-    {
+    public void autoColor() throws InterruptedException {
        /* NOT COMPLETE
          change colors every 5 second
-        private static int everyLoop; */
+        */
+        boolean isBlue = true;
+        int alternateChange = 0;
+        ElapsedTime secondsPassed = new ElapsedTime();
+        while(alternateChange < 6) {
 
-            rgbSignal.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
-
-            rgbSignal.setPattern(RevBlinkinLedDriver.BlinkinPattern.YELLOW);
-
-
+            if (secondsPassed.milliseconds() >= 5 * Math.pow(10, 3)) {
+                if (isBlue)
+                    rgbSignal.setPattern(RevBlinkinLedDriver.BlinkinPattern.YELLOW);
+                isBlue = false;
+            }else {
+                rgbSignal.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+                isBlue = true;
+            }
+              alternateChange++;
+              secondsPassed.reset();
+        }
 
 
     }
