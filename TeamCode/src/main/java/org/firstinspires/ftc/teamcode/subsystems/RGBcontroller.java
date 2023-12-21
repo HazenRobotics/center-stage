b2063d3507c875d05aaabbbcfd1cf1985715c1f3
Armaan Sidhu <armaansidhu000@icloud.com>
2023-12-21 15:16:05 -0800

    Class for the LED lights. Can be used to set color or create a pattern

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RGBcontroller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RGBcontroller.java
@@ -1,53 +1,43 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import static android.icu.lang.UProperty.MATH;
+import static android.icu.lang.UProperty.PATTERN_SYNTAX;
 import static java.lang.Thread.sleep;
 
-import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver.BlinkinPattern;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.utils.GamepadEvents;
 import org.firstinspires.ftc.teamcode.utils.RGBLights;
 
 
-public class RGBcontroller extends RGBLights{
+public class RGBcontroller extends RGBLights {
 
 
-    RGBLights rgbSignal;
+    ElapsedTime secondsPassed = new ElapsedTime();
+    BlinkinPattern[] rgbArray;
 
-    public RGBcontroller(String name,HardwareMap hw)
-    {
+    public RGBcontroller(String name, HardwareMap hw) {
         super(hw, name);
-
-    }
-
-    public void setColor(RevBlinkinLedDriver.BlinkinPattern Color) {rgbSignal.setPattern(Color);}
-
-
-    public void autoColor() throws InterruptedException {
-       /* NOT COMPLETE
-         change colors every 5 second
-        */
-        boolean isBlue = true;
-        int alternateChange = 0;
-        ElapsedTime secondsPassed = new ElapsedTime();
-        while(alternateChange < 6) {
-
-            if (secondsPassed.milliseconds() >= 5 * Math.pow(10, 3)) {
-                if (isBlue)
-                    rgbSignal.setPattern(RevBlinkinLedDriver.BlinkinPattern.YELLOW);
-                isBlue = false;
-            }else {
-                rgbSignal.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
-                isBlue = true;
-            }
-              alternateChange++;
-              secondsPassed.reset();
-        }
+        rgbArray = new BlinkinPattern[]{BlinkinPattern.BLUE, BlinkinPattern.BLUE};
 
 
     }
 
+    //Take in two parameters of color and alternate between them every second
+    public void setColor(BlinkinPattern color) {
+        super.setPattern(color);
 
+    }
 
+    public void setColor(BlinkinPattern color0, BlinkinPattern color1) {
+        secondsPassed.reset();
+        rgbArray[0] = color0;
+        rgbArray[1] = color1;
+    }
+
+    public void update() {
+       super.setPattern(rgbArray[secondsPassed.seconds() % 1 < 0.5 ? 0 : 1]);
+    }
 }
